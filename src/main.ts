60d586c78c0d85309ede83f9a6fd751f27130244
Atia-Khan <atia.khan@xloopdigital.com>
2024-09-13 11:56:23 +0500

    fixes done in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-
-  const app = await NestFactory.cconst PORT = process.env.PORT || 3000;reate(AppModule);
+  const PORT = process.env.PORT || 3000;
+  const app = await NestFactory.create(AppModule);
   await app.listen(PORT);
 }
 bootstrap();
