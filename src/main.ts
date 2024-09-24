2e54b46017dd997d6a29577a8c253ca00ad605bd
Atia-Khan <127720791+Atia-Khan@users.noreply.github.com>
2024-09-24 21:49:07 +0500

    updated CORS issue

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: ['http://localhost:3000'], 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, 
-  });
-
+  app.enableCors();
   await app.listen(PORT);
 }
 bootstrap();
